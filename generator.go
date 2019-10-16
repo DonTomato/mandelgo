d758f8449c5af81c4622617be0c4e1f3befc60c1
Michael Voronov <michael.voronov@gmail.com>
2019-10-16 22:52:38 +0300

    bug fixes

--- generator.go
+++ generator.go
@@ -10,15 +10,26 @@ type initialCondition struct {
 }
 
 func generate() {
-	const number = 100
+	const number = 255
 
 	queue := newQueue(number)
 	for i := 0; i < number; i++ {
 		queue.push(&initialCondition{z0: complex(0, float64(i)/float64(number)), index: uint8(i)})
 	}
+	done := make(chan uint8)
 	//fmt.Printf("Slice: %v\n", queue.Items)
 	for thIndex := 0; thIndex < runtime.GOMAXPROCS(0); thIndex++ {
-		go process(queue)
+		go func() {
+			for item := queue.pop(); item != nil; item = queue.pop() {
+				//fmt.Printf("Item: %v;\n", item)
+				buildFile(item.z0, item.index)
+			}
+			done <- 1
+		}()
+	}
+
+	for thIndex := 0; thIndex < runtime.GOMAXPROCS(0); thIndex++ {
+		<-done
 	}
 }
 
